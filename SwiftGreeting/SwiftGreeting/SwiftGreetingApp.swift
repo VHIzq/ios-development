dff40cfb26282a9a07c26fc04d1764cada59540b
VHIzq <vhmizq@gmail.com>
2024-02-24 20:57:58 -0600

    first commit

---
+++ SwiftGreeting/SwiftGreeting/SwiftGreetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGreetingApp.swift
+//  SwiftGreeting
+//
+//  Created by Victor Hugo on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGreetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
